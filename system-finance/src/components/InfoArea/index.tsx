d1f365fed31113f442b8a54e7d1947a0d0814dad
anemoreira <adjanead1@gmail.com>
2023-07-08 19:33:45 -0300

    refactor: code --restructuring

--- system-finance/src/components/InfoArea/index.tsx
+++ system-finance/src/components/InfoArea/index.tsx
@@ -1,46 +1,50 @@
-import * as C from './styles';
+import React from 'react';
+import * as Element from './styles';
 import { formatCurrentMonth } from '../../helpers/dateFilter';
 import { ResumeItem } from '../ResumeItem';
 
 type Props = {
-    currentMonth: string;
-    onMonthChange: (newMonth: string) => void;
-    income: number;
-    expense: number;
-}
+  currentMonth: string;
+  onMonthChange: (newMonth: string) => void;
+  income: number;
+  expense: number;
+};
 
-export const InfoArea = ({ currentMonth, onMonthChange, income, expense }: Props) => {
-    
-    const handlePrevMonth = () => {
-        let [year, month] = currentMonth.split('-');
-        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
-        currentDate.setMonth( currentDate.getMonth() - 1 );
-        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
-    }
-
-    const handleNextMonth = () => {
-        let [year, month] = currentMonth.split('-');
-        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
-        currentDate.setMonth( currentDate.getMonth() + 1 );
-        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
-    }
-
-    return (
-        <C.Container>
-            <C.MonthArea>
-                <C.MonthArrow onClick={handlePrevMonth}>⬅️</C.MonthArrow>
-                <C.MonthTitle>{formatCurrentMonth(currentMonth)}</C.MonthTitle>
-                <C.MonthArrow onClick={handleNextMonth}>➡️</C.MonthArrow>
-            </C.MonthArea>
-            <C.ResumeArea>
-                <ResumeItem title="Receitas" value={income} />
-                <ResumeItem title="Despesas" value={expense} />
-                <ResumeItem
-                    title="Balanço"
-                    value={income - expense}
-                    color={(income-expense) < 0 ? 'red' : 'green'}
-                />
-            </C.ResumeArea>
-        </C.Container>
+export const InfoArea: React.FC<Props> = ({
+  currentMonth,
+  onMonthChange,
+  income,
+  expense,
+}) => {
+  const handleMonthChange = (increment: number) => {
+    const [year, month] = currentMonth.split('-');
+    const currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
+    currentDate.setMonth(currentDate.getMonth() + increment);
+    onMonthChange(
+      `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`
     );
-}
+  };
+
+  return (
+    <Element.Container>
+      <Element.MonthArea>
+        <Element.MonthArrow onClick={() => handleMonthChange(-1)}>
+          ⬅️
+        </Element.MonthArrow>
+        <Element.MonthTitle>{formatCurrentMonth(currentMonth)}</Element.MonthTitle>
+        <Element.MonthArrow onClick={() => handleMonthChange(1)}>
+          ➡️
+        </Element.MonthArrow>
+      </Element.MonthArea>
+      <Element.ResumeArea>
+        <ResumeItem title="Receitas" value={income} />
+        <ResumeItem title="Despesas" value={expense} />
+        <ResumeItem
+          title="Balanço"
+          value={income - expense}
+          color={income - expense < 0 ? 'red' : 'green'}
+        />
+      </Element.ResumeArea>
+    </Element.Container>
+  );
+};
