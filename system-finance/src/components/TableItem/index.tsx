ea92176065a5fcd1b53d0ddb51b077f687b6f50a
anemoreira <adjanead1@gmail.com>
2023-07-08 19:38:12 -0300

    refactor: code --restructuring

--- system-finance/src/components/TableItem/index.tsx
+++ system-finance/src/components/TableItem/index.tsx
@@ -1,4 +1,4 @@
-import * as C from './styles';
+import * as Element from './styles';
 import { Item } from '../../types/Item';
 import { formatDate } from '../../helpers/dateFilter';
 import { categories } from '../../data/categories';
@@ -9,19 +9,19 @@ type Props = {
 
 export const TableItem = ({ item }: Props) => {
     return (
-        <C.TableLine>
-            <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
-            <C.TableColumn>
-                <C.Category color={categories[item.category].color}>
+        <Element.TableLine>
+            <Element.TableColumn>{formatDate(item.date)}</Element.TableColumn>
+            <Element.TableColumn>
+                <Element.Category color={categories[item.category].color}>
                     {categories[item.category].title}
-                </C.Category>
-            </C.TableColumn>
-            <C.TableColumn>{item.title}</C.TableColumn>
-            <C.TableColumn>
-                <C.Value color={categories[item.category].expense ? 'red' : 'green'}>
+                </Element.Category>
+            </Element.TableColumn>
+            <Element.TableColumn>{item.title}</Element.TableColumn>
+            <Element.TableColumn>
+                <Element.Value color={categories[item.category].expense ? 'red' : 'green'}>
                     R$ {item.value}
-                </C.Value>
-            </C.TableColumn>
-        </C.TableLine>
+                </Element.Value>
+            </Element.TableColumn>
+        </Element.TableLine>
     );
 }
